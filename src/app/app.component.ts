67aabbfd3b87bd0b9a929a7aea35682feb3ec16c
bubong5480 <williamwei5480@gmail.com>
2023-05-04 22:07:05 -0400

    Finished A-d, I'm like that

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ps6';
+  data:any = [];
+  constructor(private http: HttpClient){}
+  ngOnInit() {
+    this.http.get('/assets/mockdata.json').subscribe((response:any)=> {
+      this.data = response.data
+      console.log(this.data)
+      console.log(response)
+    })
+  }
 }
